82759661441ab5cccf6cb0f41c61f7cc759da4ae
Raginiaradhya <raginirock90@gmail.com>
2025-12-30 15:57:30 +0530

    updated msg

--- tests/testscript1.spec.js
+++ tests/testscript1.spec.js
@@ -4,4 +4,5 @@ test('login',async({page})=>
     await page.goto('https://www.amazon.in/');
     await page.waitForTimeout(2000)
     await expect(page).toHaveURL('https://www.amazon.in/')
+    console.log("hi")
 })
