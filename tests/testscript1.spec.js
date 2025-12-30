731ff919d8863b9422d351625ac8aeaa27598e4c
Raginiaradhya <raginirock90@gmail.com>
2025-12-30 15:05:07 +0530

    first commit

--- tests/testscript1.spec.js
+++ tests/testscript1.spec.js
@@ -1,5 +1,6 @@
 import{test,expect} from '@playwright/test'
 test('login',async({page})=>
 {
-    await page.goto('https://www.amazon.in/')
+    await page.goto('https://www.amazon.in/');
+    await page.waitForTimeout(2000)
 })
